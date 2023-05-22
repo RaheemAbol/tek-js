c82684ce74101d851809d36ee329b65eadad5d8a
Raheem Abolfathzadeh <raheemabol91@gmail.com>
2023-05-22 15:52:44 -0400

    logical operators/ string access / string concatination / template literal/ string comparison/ escape sequences

--- js/app.js
+++ js/app.js
@@ -6,15 +6,133 @@ console.log("hello world");*/
 
 let num = 5;
 
-let name = "john";
+let name3 = undefined;
 
-let isMonday = true;
+// console.log(typeof name3);
+
+var name = "john";
+
+let is_Monday = true;
 
 let name2 = null;
-
-let name3 = undefined;
 
 let student = {
   name: "Jim",
   age: 25,
 };
+
+let solution = 10 % 3;
+
+// console.log(solution);
+
+// let newNumber = 9;
+
+// newNumber = newNumber + 10;
+
+// newNumber += 1;
+
+// console.log(newNumber);
+
+// let newNumber = 50;
+// newNumber--; // newNumber is now equal to 10
+
+// console.log(newNumber);
+
+// let newNumber = 9;
+// let currentNumber = ++newNumber;
+// console.log("current " + currentNumber);
+
+// console.log("new " + newNumber);
+
+// let x = 5;
+// ++x;
+
+// var x = 5;
+
+// function printToConsole() {
+//   var x = 6;
+//   console.log(x);
+// }
+
+// console.log(x);
+// let x = 6;
+
+// printToConsole();
+
+// console.log(4 < 2 || 2 === 3);
+
+// let exp = !(4 < 2 || 2 === 3);
+
+// console.log(exp);
+
+// let name4 = "jim";
+
+// console.log(name4[1]);
+
+// console.log("Hello how are" + " you " + name4);
+
+// const a = "a";
+// const b = "b";
+// const c = "c";
+// console.log(c < a);
+
+// console.log("Hey" == "HEy");
+
+// const frost =
+//   "Nature’s first green is gold, " +
+//   "Her hardest hue to hold. " +
+//   "Her early leaf’s a flower; " +
+//   "But only so an hour. " +
+//   "Then leaf subsides to leaf. " +
+//   "So Eden sank to grief, " +
+//   "So dawn goes down to day. " +
+//   "Nothing gold can stay. ";
+
+// console.log(frost);
+
+// const frost =
+//   "Nature’s first green is gold, \
+// Her hardest hue to hold. \
+// Her early leaf’s a flower; \
+// But only so an hour. \
+// Then leaf subsides to leaf. \
+// So Eden sank to grief, \
+// So dawn goes down to day. \
+// Nothing gold can stay.";
+
+// console.log(frost);
+
+// console.log('They said, "this string does not need an escape!"');
+// // They said, "this string does not need an escape!"
+
+// const frost =
+//   "Nature’s first green is gold, \n\
+// Her hardest hue to hold. \n\
+// Her early leaf’s a flower; \n\
+// But only so an hour. \n\
+// Then leaf subsides to leaf. \n\
+// So Eden sank to grief, \n\
+// So dawn goes down to day. \n\
+// Nothing gold can stay.";
+
+// console.log(frost);
+
+let person = "john";
+
+let sentence = "Hey how are you ";
+
+console.log(sentence + person);
+
+console.log(`${sentence}${person} ? please let me know ${6 + 7}`);
+
+// const a = 5;
+// const b = 10;
+
+// console.log("'a' is assigned a value of \"" + a + ",\" and 'b' is assigned a value of \"" + b + ".\"\n" +
+// "Therefore, 'a' plus 'b' is equal to " + (a + b) + ".");
+
+const a = 5;
+const b = 10;
+
+console.log(`'a' is assigned a value of "${a}," and 'b' is assigned a value of "${b}."
+Therefore, 'a' plus 'b' is equal to ${a + b}.`);
