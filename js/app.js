895ad367467878fc44281b28a518eb8f6cb6f819
Raheem Abolfathzadeh <raheemabol91@gmail.com>
2023-05-22 13:05:07 -0400

    console log/ comments/ data types

---
+++ js/app.js
@@ -0,0 +1,20 @@
+/*console.log("hello world");
+console.log("hello world");
+console.log("hello world");
+console.log("hello world");
+console.log("hello world");*/
+
+let num = 5;
+
+let name = "john";
+
+let isMonday = true;
+
+let name2 = null;
+
+let name3 = undefined;
+
+let student = {
+  name: "Jim",
+  age: 25,
+};
